052e76129ae841812fb143a892c98b7e92c94115
tom <ionfury@gmail.com>
2018-08-09 23:38:59 -0400

    more formatting tests

--- src/Moons2.js
+++ src/Moons2.js
@@ -105,9 +105,8 @@ function Announce2() {
         .filter(ore => ore.value/ore.volume > BUYBACK_PRICE);
       
       if(valubleOres.length > 0) {
-        string += `\n@everyone:\n The corp needs you to mine the following ores and contract the `;
+        string += `\n@everyone:\n Contract the following ores to corp @ 350 isk/m3: `;
         string += valubleOres.map(ore => `**${ore.product}**`).join(', ');
-        string += ' to corp at 350 isk/m3: '
       }
 
       moons.forEach(moon => {
